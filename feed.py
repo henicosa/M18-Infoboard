3e0c04e5911ac9fbfbcf88a82ad6ee2aeb4a8010
henicosa <ludwig.david.lorenz@uni-weimar.de>
2025-03-17 19:28:51 +0100

    add 2 months deadline for articles

--- feed.py
+++ feed.py
@@ -2,6 +2,8 @@ import json
 import crawler.pinnwand
 import crawler.dummy
 
+import datetime
+
 def fetch_crawlers():
     # Now add here all the crawlers
     entries = []
@@ -12,6 +14,9 @@ def fetch_crawlers():
     # ...
     # sort entries according to their publication time
     entries.sort(key=lambda x: x["pubUnix"], reverse=True)
+
+    # filter all entries that are older than 1 month
+    entries = [entry for entry in entries if entry["pubUnix"] > datetime.datetime.now().timestamp() - 30*24*60*60]
 
     with open("feed.json", "w") as feed:
         json.dump(entries, feed, indent=4)
